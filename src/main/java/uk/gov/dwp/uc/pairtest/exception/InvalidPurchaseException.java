25f7aa6ce829c31af19b324b975e193dd7b0230a
negaonka <neha.n.gaonkar@gmail.com>
2023-04-22 22:21:18 +0100

    A ticket purchasing system

---
+++ src/main/java/uk/gov/dwp/uc/pairtest/exception/InvalidPurchaseException.java
@@ -0,0 +1,10 @@
+package uk.gov.dwp.uc.pairtest.exception;
+
+public class InvalidPurchaseException extends RuntimeException {
+
+	public InvalidPurchaseException(String errors) {
+		// TODO Auto-generated constructor stub
+		super(errors);
+	}
+
+}
